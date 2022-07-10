d564817c1875eeb7e83ebe56cecec4d39f9d2d6c
postman17 <frompostal@gmail.com>
2022-07-10 18:09:38 +0500

    added good view for backup admin

--- project/backup/admin/backup.py
+++ project/backup/admin/backup.py
@@ -7,3 +7,5 @@ from helpers.admin import CreatedAtAndUpdatedAtAdminMixin
 @admin.register(Backup)
 class BackupAdmin(CreatedAtAndUpdatedAtAdminMixin, admin.ModelAdmin):
     list_display = ("id", "name", "status", "owner", "is_active", "updated_at", "created_at")
+    filter_horizontal = ('services', "backups", "clients", "files")
+    raw_id_fields = ('periodic_task', )
